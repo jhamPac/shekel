919b397aee126788dbe6ba97c91b734b0e9c2a7b
jhamPac <cloud@kevia.me>
2021-10-22 23:59:50 -0700

    refactor: move code for better readability

--- packages/cia/app.js
+++ packages/cia/app.js
@@ -1,19 +1,18 @@
 require('dotenv').config()
-
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var blocksRouter = require('./routes/blocks')
-
+// app
 var app = express()
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+// routes
+var blocksRouter = require('./routes/blocks')
 app.use('/blocks/api/v1', blocksRouter)
 
 module.exports = app
