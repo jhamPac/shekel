115cadd7ece094d9336244c87993750cca80d2f7
jhamPac <cloud@kevia.me>
2021-10-25 19:15:47 -0700

    feat: add search for asset address owner

--- packages/cia/routes/assets.js
+++ packages/cia/routes/assets.js
@@ -2,6 +2,22 @@ const express = require("express")
 const router = express.Router()
 const cardano = require("../cardano/cardano.js")
 
+router.get("/:asset/addresses", async (req, res, next) => {
+    const { asset } = req.params
+    try {
+        const result = await cardano.assetsAddresses(asset)
+        res.json({
+            data: [result],
+            error: false,
+        })
+    } catch (err) {
+        res.json({
+            data: [],
+            error: true,
+        })
+    }
+})
+
 router.get("/:asset/history", async (req, res, next) => {
     const { asset } = req.params
     try {
