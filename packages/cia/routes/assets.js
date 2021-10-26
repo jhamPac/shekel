9fcff314c3839448b4acc8a8d62bc913de4fd413
jhamPac <cloud@kevia.me>
2021-10-25 18:58:24 -0700

    feat: add search asset history

--- packages/cia/routes/assets.js
+++ packages/cia/routes/assets.js
@@ -1,7 +1,23 @@
 const express = require("express")
 const router = express.Router()
 const cardano = require("../cardano/cardano.js")
-/* GET home page. */
+
+router.get("/:asset/history", async (req, res, next) => {
+    const { asset } = req.params
+    try {
+        const result = await cardano.assetsHistory(asset)
+        res.json({
+            data: [result],
+            error: false,
+        })
+    } catch (err) {
+        res.json({
+            data: [],
+            error: true,
+        })
+    }
+})
+
 router.get("/:asset", async (req, res, next) => {
     const { asset } = req.params
     try {
