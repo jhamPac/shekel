0d70ea5b2787579b725063a54b0918928993d15a
jhamPac <cloud@kevia.me>
2021-11-06 16:59:25 -0700

    feat: utilites to convert ipfs urls into https

--- packages/cia/routes/accounts.js
+++ packages/cia/routes/accounts.js
@@ -2,8 +2,24 @@ const express = require("express")
 const router = express.Router()
 const cardano = require("../cardano/cardano.js")
 
+const convertIPFSURL = url => {
+    path = pluckIPFSpath(url)
+    return "https://ipfs.io/ipfs/" + path
+}
+
+const pluckIPFSpath = url => url.replace(/ipfs/g, "/").split("/").slice(-1)[0]
+const createAssetUIData = (a, c) => {
+    const { image, files, ...x } = c
+    const z = {
+        ...x,
+        imageURL: convertIPFSprotocol(image),
+    }
+    return a.concat(z)
+}
+
 router.get("/:stakeAddress/addresses/assets", async (req, res) => {
     const { stakeAddress } = req.params
+
     try {
         const results = await cardano.accountsAddressesAssets(stakeAddress)
 
@@ -12,7 +28,9 @@ router.get("/:stakeAddress/addresses/assets", async (req, res) => {
             xs.push(cardano.assetsById(r.unit))
         })
 
-        const zs = await Promise.all(xs)
+        const ys = await Promise.all(xs)
+
+        const zs = ys.map(d => d.onchain_metadata).reduce()
 
         res.json({
             data: zs,
