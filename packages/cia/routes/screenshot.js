3d78e21fe71cf02bd8e1858924cb58e8d87e5f55
jhamPac <cloud@kevia.me>
2021-11-12 17:54:39 -0700

    feat: take screenshot of tweet complete

--- packages/cia/routes/screenshot.js
+++ packages/cia/routes/screenshot.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const puppeteer = require("puppeteer")
+const path = require("path")
 
 router.post("/create", async (req, res, next) => {
     const { tweetId } = req.query
@@ -9,26 +10,30 @@ router.post("/create", async (req, res, next) => {
     let page
 
     try {
+        // set up a headless browser
         browser = await puppeteer.launch({
             headless: true,
             args: ["--disable-notifications"],
         })
 
+        // creates a tab
         page = await browser.newPage()
 
+        // setting this to Chrome is important, the default is HeadlessChrome which will be blocked
         await page.setUserAgent(
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
         )
 
+        // navigate to URL
         await page.goto(twitterURL + tweetId, { waitUntil: "networkidle0" })
 
+        // tweets are wrapped an article element
         await page.waitForSelector('article[role="article"]')
 
+        // grab the tweet
         const tweet = await page.$('article[role="article"]')
 
-        const tweetBodyText = await page.$('article[role="article"] div[lang="en"]')
-
-        console.log(tweetBodyText, "THE TWEET")
+        await tweet.screenshot({ path: path.join(__dirname, "test.png") })
 
         res.json({
             data: "Success",
