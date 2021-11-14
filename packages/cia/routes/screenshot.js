c7091bac6e8b1ddc29c5562e7557211ad352ff36
jhamPac <cloud@kevia.me>
2021-11-14 14:21:26 -0700

    feat: move screenshot related code to its own module

--- packages/cia/routes/screenshot.js
+++ packages/cia/routes/screenshot.js
@@ -1,60 +1,23 @@
-const path = require("path")
 const express = require("express")
 const router = express.Router()
-const puppeteer = require("puppeteer")
-const { uniqueNamesGenerator, colors, countries } = require("unique-names-generator")
+
+const screenshotTweet = require("../twitter/ss.js")
 
 router.post("/create", async (req, res) => {
     const { tweetId } = req.query
-    const twitterURL = "https://twitter.com/anyuser/status/"
 
-    let browser
-    let page
+    const { ok } = screenshotTweet(tweetId)
 
-    try {
-        // set up a headless browser
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ["--disable-notifications"],
-        })
-
-        // creates a tab
-        page = await browser.newPage()
-
-        // setting this to Chrome is important, the default is HeadlessChrome which will be blocked
-        await page.setUserAgent(
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-        )
-
-        // navigate to URL
-        await page.goto(twitterURL + tweetId, { waitUntil: "networkidle0" })
-
-        // tweets are wrapped an article element
-        await page.waitForSelector('article[role="article"]')
-
-        // grab the tweet
-        const tweet = await page.$('article[role="article"]')
-
-        // take the screenshot
-        const randomName = uniqueNamesGenerator({
-            dictionaries: [colors, countries],
-            separator: "-",
-        }).toLowerCase()
-
-        const fileName = randomName + "_" + tweetId + ".jpg"
-
-        await tweet.screenshot({
-            path: path.join(__dirname, "../screenshots/" + fileName),
-        })
-
+    if (ok) {
         res.json({
             data: "Success",
             error: false,
         })
-    } catch (err) {
-        console.log(err)
-    } finally {
-        await browser.close()
+    } else {
+        res.json({
+            data: [],
+            error: true,
+        })
     }
 })
 
