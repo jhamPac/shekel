2d9bd4bb057759945d74c656071e884d9b879b47
jhamPac <cloud@kevia.me>
2021-11-29 19:32:11 -0700

    refactor: change dashboard for twitter login

---
+++ packages/ui/src/Dashboard/Explorer.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react"
+import { useQuery } from "react-query"
+import AssetList from "../AssetList"
+
+const Explorer = () => {
+    const [epoch, setEpoch] = useState(null)
+    const [stakeAddress, setStakeAddress] = useState("")
+    const [assets, setAssets] = useState([])
+    const {
+        isLoading,
+        error,
+        data: result,
+    } = useQuery("blockTip", () =>
+        fetch("http://localhost:3000/api/v1/blocks/tip").then(res => res.json())
+    )
+
+    useEffect(() => {
+        if (result && result.data.length > 0) {
+            const block = result.data[0]
+            setEpoch(block.epoch)
+        }
+    }, [result])
+
+    const getAssetsByStake = async s => {
+        try {
+            const p = new URLSearchParams({
+                stakeAddress: s,
+            })
+
+            const resp = await fetch(
+                "http://localhost:3000/api/v1/accounts/stake/assets?" + p
+            )
+            const { data } = await resp.json()
+            setAssets(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const submitHandler = e => {
+        e.preventDefault()
+        getAssetsByStake(stakeAddress)
+    }
+
+    const changeHandler = e => {
+        setStakeAddress(e.target.value)
+    }
+
+    const epochText = (epoch, error, isLoading) => {
+        if (isLoading) return "..."
+
+        if (error) return "Could not fetch latest block."
+
+        if (epoch !== null) return epoch
+    }
+
+    return (
+        <div id="dashboard">
+            <h1>CNFT Inspector</h1>
+            <h2>{`Epoch: ${epochText(epoch, error, isLoading)}`}</h2>
+            <div style={{ marginBottom: "32px" }}>
+                <form onSubmit={submitHandler}>
+                    <input type="text" value={stakeAddress} onChange={changeHandler} />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <div>{assets.length > 0 ? <AssetList list={assets} /> : null}</div>
+        </div>
+    )
+}
+
+export default Explorer
