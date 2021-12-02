6c8f4a4e045a7da32725016b4512c870d1cacf06
jhamPac <cloud@kevia.me>
2021-12-01 19:32:49 -0700

    feat: save authenticated users

--- packages/cia/passport/setup.js
+++ packages/cia/passport/setup.js
@@ -1,5 +1,6 @@
 const passport = require("passport")
 const TwitterStrategy = require("passport-twitter").Strategy
+const User = require("../models/User.js")
 
 passport.use(
     new TwitterStrategy(
@@ -8,8 +9,25 @@ passport.use(
             consumerSecret: process.env.TWITTER_API_SECRET,
             callbackURL: "/auth/twitter/redirect",
         },
-        () => {
-            console.log("something happened")
+        async (token, tokenSecret, profile, done) => {
+            const currentUser = await User.findOne({
+                twitterId: profile._json.id_str,
+            })
+
+            if (!currentUser) {
+                const newUser = await new User({
+                    name: profile._json.name,
+                    handle: profile._json.screen_name,
+                    twitterId: profile._json.id_str,
+                    profileImageUrl: profile._json.profile_image_url,
+                }).save()
+
+                if (newUser) {
+                    done(null, newUser)
+                }
+            }
+
+            done(null, currentUser)
         }
     )
 )
