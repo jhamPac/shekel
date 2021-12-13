c0c02264a928e276cacb96e2f4a8a082ab4695b5
jhamPac <cloud@kevia.me>
2021-12-12 20:15:34 -0700

    feat: create containers to align items

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,21 +1,40 @@
 import { QueryClient, QueryClientProvider } from "react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import styled from "styled-components"
 import Dashboard from "./Dashboard"
 
 const qc = new QueryClient()
 
 const App = () => {
     return (
-        <div id="app-container">
-            <QueryClientProvider client={qc}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                    </Routes>
-                </BrowserRouter>
-            </QueryClientProvider>
-        </div>
+        <MainContainer id="app-container">
+            <ContentContainer>
+                <QueryClientProvider client={qc}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                        </Routes>
+                    </BrowserRouter>
+                </QueryClientProvider>
+            </ContentContainer>
+        </MainContainer>
     )
 }
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+`
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    height: 100%;
+    max-width: 1200px;
+`
 
 export default App
